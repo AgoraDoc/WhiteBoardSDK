17c7081b2edf581cfa82dce3017b80107155a06b
陈婷 <chenting@agora.io>
2021-04-16 23:28:10 +0800

    Add EN java files

--- Android--EN/domain/ViewMode.java
+++ Android--EN/domain/ViewMode.java
@@ -3,40 +3,40 @@ package com.herewhite.sdk.domain;
 import com.google.gson.annotations.SerializedName;
 
 /**
- * Created by buhe on 2018/8/11.
- */
-
-/**
- * 视角模式。
+ * The user's view mode.
  */
 public enum ViewMode {
     /**
-     * （默认）自由模式。
-     * <p>
-     * 该模式下用户可以主动调整视角，不受其他用户视角模式设置的影响，也不会影响其他用户的视角模式设置。
+     * (Default) Free mode.
      *
-     * @note 当房间内不存在视角为主播模式的用户时，所有用户的视角都默认为自由模式。
+     * In this mode, the user can freely adjust their own view. Each user's view setting does not affect others.
+     *
+     * @note
+     * When there is no host in the room, all users are in `Freedom` view mode by default.
      */
     @SerializedName("freedom")
     Freedom,
     /**
-     * 跟随模式。
-     * <p>
-     * 该模式下用户的视角会跟随主播的视角。
+     * Follower mode.
+     *
+     * In this mode, the user's view follows the view of the host.
      *
      * @note
-     * - 当一个用户的视角设置为主播模式后，房间内其他所有用户（包括新加入房间的用户）的视角会被自动设置为跟随模式。
-     * - 跟随模式的用户进行白板操作时，其视角会自动切换为自由模式。
-     * 如有需要，可以调用 {@link disableOperations(boolean) disableOperations} 禁止用户操作，以锁定用户的视角模式。
+     * - When a user’s view mode is set as `Broadcaster`, the view mode of other users in the room (including newly-joined users) automatically changes to `Follower`.
+     * - When a user in the `Follower` view mode operates the whiteboard, their view mode automatically swiches to the `Freedom` mode.
+     * If needed, you can call {@link disableOperations(boolean) disableOperations} to disable the user from operating the whiteboard, so as to lock their view mode.
      */
     @SerializedName("follower")
     Follower,
     /**
-     * 主播模式。
+     * Host mode.
+     *
+     * In this mode, the user can freely adjust their view and synchronize thier view to other users in the room.
      * 该模式下用户可以主动调整视角，并将自己的视角同步给房间内所有其他用户。
      *
-     * @note - 每个房间只能有一个主播模式视角的用户。
-     * - 当一个用户的视角设置为主播模式后，房间内所有其他用户（包括新加入房间的用户）的视角会被自动设置为跟随模式。
+     * @note
+     * - Each room can have only one user in the `host` view mode.
+     * - - When a user’s view mode is set as `Broadcaster`, the view mode of other users in the room (including newly-joined users) automatically changes to `Follower`.
      */
     @SerializedName("broadcaster")
     Broadcaster
