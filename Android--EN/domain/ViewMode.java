302c3192277f1d102c9b911095218356f5621836
陈婷 <chenting@agora.io>
2021-04-26 18:29:48 +0800

    Update ViewMode.java

--- Android--EN/domain/ViewMode.java
+++ Android--EN/domain/ViewMode.java
@@ -7,12 +7,13 @@ import com.google.gson.annotations.SerializedName;
  */
 public enum ViewMode {
     /**
-     * (Default) Free mode.
+     * (Default) Freedom mode.
      *
-     * In this mode, the user can freely adjust their own view. Each user's view setting does not affect others.
+     * In this mode, the user can freely adjust their own view. Each user's view setting does not affect the view settings of other users.
      *
      * @note
      * When there is no host in the room, all users are in `Freedom` view mode by default.
+     *
      */
     @SerializedName("freedom")
     Freedom,
@@ -22,8 +23,8 @@ public enum ViewMode {
      * In this mode, the user's view follows the view of the host.
      *
      * @note
-     * - When a user’s view mode is set as `Broadcaster`, the view mode of other users in the room (including newly-joined users) automatically changes to `Follower`.
-     * - When a user in the `Follower` view mode operates the whiteboard, their view mode automatically switches to the `Freedom` mode.
+     * - When one user’s view mode is set as `Broadcaster`, the view mode of the other users in the room (including users that join subsequently) automatically changes to `Follower`.
+     * - When a user in `Follower` view mode operates the whiteboard, their view mode automatically switches to `Freedom` mode.
      * If needed, you can call {@link disableOperations(boolean) disableOperations} to disable the user from operating the whiteboard, so as to lock their view mode.
      */
     @SerializedName("follower")
@@ -31,12 +32,11 @@ public enum ViewMode {
     /**
      * Host mode.
      *
-     * In this mode, the user can freely adjust their view and synchronize their view to other users in the room.
-     * 该模式下用户可以主动调整视角，并将自己的视角同步给房间内所有其他用户。
+     * In this mode, the user can freely adjust their view, and every other user in the room can only duplicate the view of the host.
      *
      * @note
-     * - Each room can have only one user in the `host` view mode.
-     * - - When a user’s view mode is set as `Broadcaster`, the view mode of other users in the room (including newly-joined users) automatically changes to `Follower`.
+     * - Each room can have only one user in `Broadcaster` view mode.
+     * - When a user’s view mode is set as `Broadcaster`, the view mode of every other user in the room (including users that join subsequently) is automatically set as `Follower`.
      */
     @SerializedName("broadcaster")
     Broadcaster
