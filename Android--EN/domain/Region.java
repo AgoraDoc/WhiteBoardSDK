a15b6977787cb1822b95a90b7e12ffba9542fd62
陈婷 <chenting@agora.io>
2021-05-10 17:50:19 +0800

    Update Region.java
    add more data centers

--- Android--EN/domain/Region.java
+++ Android--EN/domain/Region.java
@@ -7,13 +7,38 @@ import com.google.gson.annotations.SerializedName;
  */
 public enum Region {
     /**
-     * `cn`: Hangzhou, China.
+     * `cn` (`cn-hz`): Hangzhou, China.
+     *
+     * This data center provides services to the areas that are not covered by other data centers.
      */
-    //@SerializedName("cn-hz")
+    // @SerializedName("cn-hz")
     cn,
     /**
-     * `us`: Silicon Valley, US.
+     * `us` (`us-sv`): Silicon Valley, US.
+     *
+     * This data center provides services to North America and South America.
      */
-    //@SerializedName("us-sv")
-    us;
+    // @SerializedName("us-sv")
+    us,
+    /**
+     * `sg`: Singapore.
+     *
+     * This data center provides services to Singapore, East Asia, and Southeast Asia.
+     */
+    // @SerializedName("sg")
+    sg,
+    /**
+     * `in_mum`: Mumbai, India.
+     *
+     * This data center provides services to India.
+     */
+    // @SerializedName("in-mum")
+    in_mum,
+    /**
+     * `gb_lon`: London, England.
+     *
+     * This data center provides services to Europe.
+     */
+    // @SerializedName("gb-lon")
+    gb_lon;
 }
